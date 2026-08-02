5ea1cfd9134821aefed6d770daec14c23fbd6da7
Androlax2 <theo.benoit16@gmail.com>
2026-08-02 12:33:30 +0200

    refactor(repo): remove typescript config package and css styles

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import "../styles.css";
+import "@opusline/ui/globals.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
