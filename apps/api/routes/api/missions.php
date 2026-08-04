84a364c28d40e6e7b334bb08441ed0da7899cb8f
Androlax2 <theo.benoit16@gmail.com>
2026-08-04 14:33:04 +0200

    feat(api): add routes for mission document management
    Introduce routes for managing mission documents, including listing, uploading, updating, downloading, and deletion.

--- apps/api/routes/api/missions.php
+++ apps/api/routes/api/missions.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Missions\Controllers\MissionController;
+use App\Http\Missions\Controllers\MissionDocumentController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->scopeBindings()->group(function (): void {
@@ -18,4 +19,20 @@ Route::middleware('auth:sanctum')->scopeBindings()->group(function (): void {
     Route::delete('/clients/{client}/missions/{mission}', [MissionController::class, 'destroy'])
         ->whereNumber(['client', 'mission'])
         ->name('deleteMission');
+
+    Route::get('/clients/{client}/missions/{mission}/documents', [MissionDocumentController::class, 'index'])
+        ->whereNumber(['client', 'mission'])
+        ->name('listMissionDocuments');
+    Route::post('/clients/{client}/missions/{mission}/documents', [MissionDocumentController::class, 'store'])
+        ->whereNumber(['client', 'mission'])
+        ->name('uploadMissionDocument');
+    Route::put('/clients/{client}/missions/{mission}/documents/{document}', [MissionDocumentController::class, 'update'])
+        ->whereNumber(['client', 'mission', 'document'])
+        ->name('updateMissionDocument');
+    Route::get('/clients/{client}/missions/{mission}/documents/{document}/download', [MissionDocumentController::class, 'download'])
+        ->whereNumber(['client', 'mission', 'document'])
+        ->name('downloadMissionDocument');
+    Route::delete('/clients/{client}/missions/{mission}/documents/{document}', [MissionDocumentController::class, 'destroy'])
+        ->whereNumber(['client', 'mission', 'document'])
+        ->name('deleteMissionDocument');
 });
