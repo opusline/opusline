21a4a5c39a76b8274a1702c13d1375f16d11e8b6
Théo Benoit <39646949+Androlax2@users.noreply.github.com>
2026-08-04 18:26:30 +0200

    feat(api): localize app with French translations (#32)

---
+++ apps/api/lang/fr/missions.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'end_client_required_for_intermediary' => 'Un client final est requis lorsque le client facturé est un intermédiaire.',
+    'end_client_only_for_intermediary' => 'Un client final ne peut être renseigné que lorsque le client facturé est un intermédiaire.',
+    'rate_forbidden_for_internal' => 'Les missions d\'un client interne ne sont pas facturables.',
+    'rounding_forbidden_for_fixed' => 'L\'arrondi ne s\'applique pas aux missions au forfait.',
+];
